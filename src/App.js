6bb09d7cf90592c0af762fd9c71bc22c8e860c1e
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-04-11 05:59:04 +0900

    [add]회원가입 툴 생성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IntroPage from "./components/Intro/IntroPage";
 import LoginPage from "./components/Login/LoginPage";
-
+import SignUpPage from "./components/SignUp/SignUpPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<IntroPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
