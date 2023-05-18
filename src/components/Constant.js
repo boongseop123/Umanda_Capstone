058fb6bc43f5f211fe025f23d2a993b5317aa775
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-05-19 03:25:53 +0900

    [add]코스선택 버튼 만들고 데이터 저장

--- src/components/Constant.js
+++ src/components/Constant.js
@@ -1,5 +1,5 @@
 export const API_URL =
-  "http://ec2-3-34-183-172.ap-northeast-2.compute.amazonaws.com:8080";
+  "http://ec2-3-34-183-172.ap-northeast-2.compute.amazonaws.com";
 
 export const API_URL_AI =
-  "http://ec2-3-38-211-61.ap-northeast-2.compute.amazonaws.com:5000";
+  "http://ec2-52-79-234-48.ap-northeast-2.compute.amazonaws.com:5000";
