62c76113a3ff892c597277a16e86b1ba39a7fd25
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-05-30 18:43:55 +0900

    [add]만남 지도 게인페이지 생성

--- src/components/MapChat/MapChat.js
+++ src/components/MapChat/MapChat.js
@@ -10,11 +10,11 @@ const MapChat = () => {
   const navigate = useNavigate();
 
   const goBack = () => {
-    navigate("/accompany");
+    navigate("/main");
   };
 
   const mapStyles = {
-    height: "100vh",
+    height: "80vh",
     width: "100%",
   };
 
@@ -35,11 +35,19 @@ const MapChat = () => {
 
   /* Modal 관련 state와 함수들 */
 
-  const [isModalOpen1, setIsModalOpen1] = useState(false);
-  const [isModalOpen2, setIsModalOpen2] = useState(false);
-  const [selectedMarker, setSelectedMarker] = useState(null);
-  const GoogleApi = "AIzaSyD6HTEklvN8AWBgtd_RdesV5c2PHllWu - Q";
+  // 초기 상태 설정
+  const initialMarker =
+    JSON.parse(localStorage.getItem("selectedMarker")) || null;
+  const initialModal1 =
+    JSON.parse(localStorage.getItem("isModalOpen1")) || false;
+  const initialModal2 =
+    JSON.parse(localStorage.getItem("isModalOpen2")) || false;
 
+  const [selectedMarker, setSelectedMarker] = useState(initialMarker);
+  const [isModalOpen1, setIsModalOpen1] = useState(initialModal1);
+  const [isModalOpen2, setIsModalOpen2] = useState(initialModal2);
+
+  // Marker 클릭 이벤트 핸들러
   const handleMarkerClick1 = (marker) => {
     setSelectedMarker(marker);
     setIsModalOpen1(true);
@@ -52,14 +60,24 @@ const MapChat = () => {
     setIsModalOpen1(false); // Ensure the first modal is closed when the second one opens
   };
 
+  // Modal 닫기 이벤트 핸들러
   const handleCloseModal = () => {
+    setSelectedMarker(null);
     setIsModalOpen1(false);
     setIsModalOpen2(false);
   };
 
+  // useEffect를 사용하여 마커와 모달 상태를 localStorage에 저장
+  useEffect(() => {
+    localStorage.setItem("selectedMarker", JSON.stringify(selectedMarker));
+    localStorage.setItem("isModalOpen1", JSON.stringify(isModalOpen1));
+    localStorage.setItem("isModalOpen2", JSON.stringify(isModalOpen2));
+  }, [selectedMarker, isModalOpen1, isModalOpen2]);
+  const GoogleApi = "AIzaSyD6HTEklvN8AWBgtd_RdesV5c2PHllWu-Q";
+
   /* 임의의 위도, 경도 설정 */
-  const myposition = { lat: 51.529628, lng: -0.123136 };
-  const [location, setLocation] = useState({ myposition });
+  const londonPosition = { lat: 51.5074, lng: -0.1278 }; // 런던의 위도와 경도
+  const [location, setLocation] = useState(londonPosition);
 
   const position1 = { lat: 51.526978, lng: -0.114998 };
   const position2 = { lat: 51.521491, lng: -0.130416 };
@@ -76,7 +94,7 @@ const MapChat = () => {
           {window.google && (
             <>
               <Marker
-                position={myposition}
+                position={location}
                 icon={{
                   path: window.google.maps.SymbolPath.CIRCLE,
                   fillColor: "red",
--- src/components/MapChat/Modal/MapModal2.js
+++ src/components/MapChat/Modal/MapModal2.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 
 import styles from "../Modal/MapModal.module.scss";
 import chat from "../resources/chat.png";
+import othercourse from "../../Chatting/ChatFooter/button.png";
+
 const MapModal = () => {
   const navigate = useNavigate();
   const moveToMeet = () => {
-    navigate("/meet");
+    navigate("/chatPage/:username");
   };
+
+  const [showModal, setShowModal] = useState(false); // 상태 변수 추가
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  // localStorage에서 데이터 가져오기
+  const combinedSelectedSpotsArray = JSON.parse(
+    localStorage.getItem("combinedSelectedSpotsArray")
+  );
+
   return (
     <div className={styles.container}>
       <div
@@ -19,21 +37,57 @@ const MapModal = () => {
         }}
       >
         <div className={styles.profile}></div>
-        <div className={styles.name} style={{ margin: "20px 0" }}>
-          <span>kop981020</span>
+        <div className={styles.name}>
+          <span className={styles.name}>신지훈</span>
           <br />
           <br />
-          <span style={{ color: "gray" }}>24세 ∙ 남성</span>
+          <span style={{ color: "gray" }}>26세 ∙ 남성</span>
           <br />
-          <span className={styles.feature}>#문화 #엑티비티</span>
+          <span className={styles.feature}>#해외축구 #포토스팟</span>
         </div>
         <div>
           <button className={styles.chatButton} onClick={moveToMeet}>
-            <p className={styles.spanChat}>채팅</p>
-            <img className={styles.chatImg} src={chat}></img>
+            <p className={styles.spanChat}>채팅하기</p>
+            <img className={styles.chatImg} src={chat} alt="Chat" />
+          </button>
+          <button className={styles.chatButton1} onClick={openModal}>
+            <p className={styles.spanChat1}>여행코스 보기</p>
+            <img
+              className={styles.chatImg}
+              src={othercourse}
+              alt="Other Course"
+            />
           </button>
         </div>
       </div>
+
+      {/* 모달 */}
+      {showModal && (
+        <div className={styles.modalContainer}>
+          <div className={styles.modal}>
+            <span className={styles.close} onClick={closeModal}>
+              &times;
+            </span>
+            <div className={styles.detail}>
+              신지훈님의 여행코스입니다! <br></br>
+              <span className={styles.similar}>(유사도:66%)</span>
+              <div className={styles.feature}>#해외축구 #포토스팟</div>
+            </div>
+            <div className={styles.imageContainer}>
+              {combinedSelectedSpotsArray.map((item, index) => (
+                <div key={index}>
+                  <img
+                    className={styles.spotimg}
+                    src={item.URI}
+                    alt={item.spot}
+                  />
+                  <p>{item.spot}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
